2f5bd5c4f0b1ba4233e060bcdb721da17d077741
12944qwerty <qwerty12944qwerty@gmail.com>
2025-07-14 13:23:26 -0500

    Initial commit

---
+++ Spaced/SpacedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacedApp.swift
+//  Spaced
+//
+//  Created by Krish Shah on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SpacedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpacedTests/SpacedTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpacedTests.swift
+//  SpacedTests
+//
+//  Created by Krish Shah on 7/14/25.
+//
+
+import Testing
+@testable import Spaced
+
+struct SpacedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
